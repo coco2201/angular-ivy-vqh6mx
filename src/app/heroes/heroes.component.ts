3c6c5ec871a44bb7e9a13cb9e10747820b467ffc
coco2201 <coco2201@gmail.com>
2022-11-30 13:48:17 +0700

    add full example

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,27 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = { name: 'Windstorm', id: 1 };
-  heroes = [];
-  constructor(private heroService: HeroService) {}
+    heroes: Hero[] = [];
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+    constructor(private heroService: HeroService) { }
 
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  public getHeroes() {
-    this.heroes = this.heroService.getHeroes();
-  }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
+    }
+
+    add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.heroService.addHero({ name } as Hero)
+            .subscribe(hero => {
+                this.heroes.push(hero);
+            });
+    }
+
+    delete(hero: Hero): void {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        this.heroService.deleteHero(hero.id).subscribe();
+    }
+
 }
